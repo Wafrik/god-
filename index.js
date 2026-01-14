d25182b8c0f64302aca21f25e5f11df89288e5f5
Wafrik <israelkdi@gmail.com>
2026-01-14 02:54:53 +0100

    index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const MATCHMAKING_CONFIG = {
 };
 
 const UPDATE_CONFIG = {
-  force_update: false,
+  force_update: true,
   min_version: "1.1.0",
   latest_version: "1.2.0",
   update_url: "https://play.google.com/store/apps/details?id=com.dogbale.wafrik"
@@ -1982,3 +1982,4 @@ process.on('SIGINT', () => {
 });
 
 startServer();
+
