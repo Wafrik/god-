2db3f4249f295a137aadfb92cf56407b16f73bbe
Wafrik <israelkdi@gmail.com>
2026-01-27 14:27:35 +0100

    Update dbPool.js

--- dbPool.js
+++ dbPool.js
@@ -1,1 +1,13 @@
+const { Pool } = require('pg');
 
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
+});
+
+if (!process.env.DATABASE_URL) {
+  console.error('DATABASE_URL non définie');
+  process.exit(1);
+}
+
+module.exports = pool;
